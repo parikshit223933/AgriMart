e7cd575620ffa2d4d4af5a2da16efe26c9375575
chandraprakash <chandraprak33@gmail.com>
2020-06-25 18:45:17 +0530

    edit and delete product

--- controllers/product_controller.js
+++ controllers/product_controller.js
@@ -43,3 +43,42 @@ module.exports.showPageProduct = async (req, res)=>
       console.log(`there is an errr ${err}`);
   } 
 }
+module.exports.editProduct = async (req, res)=>
+{
+    try{
+        var product = await Product.findById(req.params.product_id);
+        var options =
+        {
+            title: "AgriMart",
+            product: product    
+        };
+        return res.render('products/editProduct', options);
+    }
+    catch(err){
+        console.log(`there is an errr ${err}`);
+    }
+}
+module.exports.updateProduct = async (req, res)=>
+{
+    try{
+        var updateProduct = req.body.product;
+        var product = await Product.findByIdAndUpdate(req.params.product_id, updateProduct) ;
+        console.log("******NEW PRODUCT******", product);
+        return res.redirect('/product/'+ req.params.product_id);
+    }
+    catch(err){
+        console.log(`there is an errr ${err}`);
+    } 
+}
+
+module.exports.deleteProduct = async (req, res)=>
+{
+    try{
+      await  Product.findByIdAndDelete(req.params.product_id);
+      console.log("Succesfully product deleted")
+      res.redirect('/');
+    }
+    catch(err){
+        console.log(`there is an errr ${err}`);
+    } 
+}
--- routes/new_product_router.js
+++ routes/new_product_router.js
@@ -4,6 +4,8 @@ const product_controller=require('../controllers/product_controller');
 
 router.get('/new', product_controller.newProduct);
 router.get('/:product_id', product_controller.showPageProduct);
-
+router.get('/:product_id/edit', product_controller.editProduct);
+router.put('/:product_id/update', product_controller.updateProduct);
+router.delete('/:product_id/delete', product_controller.deleteProduct);
 
 module.exports=router;
