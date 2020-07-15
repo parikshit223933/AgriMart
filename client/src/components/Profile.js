cb8218c89e6b4341ac93c0e81743713a034d31f3
Parikshit Singh <pk223933@gmail.com>
2020-07-15 08:47:33 +0530

    profile bottom structure created

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -97,7 +97,7 @@ class Profile extends React.Component {
 							<div className="profile-top">
 								<div className="title-name mt-3">
 									<h1>John Wick</h1>
-                                    <div className="hl two"></div>
+									<div className="hl two"></div>
 								</div>
 								<div className="name-subtitle">
 									<h4>
@@ -105,13 +105,18 @@ class Profile extends React.Component {
 									</h4>
 								</div>
 								<div className="bookmark">
-									<button type="button" className="btn btn-light">
+									<button
+										type="button"
+										className="btn btn-light"
+									>
 										<i className="fas fa-bookmark text-secondary"></i>{" "}
 										Bookmark
 									</button>
 								</div>
 								<div className="rankings">
-									<p className="text-muted rating mb-0 mt-4">Rating</p>
+									<p className="text-muted rating mb-0 mt-4">
+										Rating
+									</p>
 									<div className="stars">
 										<span>860 Upvotes</span>{" "}
 										<i className="fas fa-star"></i>
@@ -119,6 +124,88 @@ class Profile extends React.Component {
 										<i className="fas fa-star"></i>
 										<i className="fas fa-star"></i>
 										<i className="fas fa-star-half-alt"></i>
+									</div>
+								</div>
+							</div>
+							<div className="profile-bottom mt-5">
+								<ul
+									className="nav nav-tabs"
+									id="myTab"
+									role="tablist"
+								>
+									<li
+										className="nav-item"
+										role="presentation"
+									>
+										<a
+											className="nav-link active bg-light"
+											id="home-tab"
+											data-toggle="tab"
+											href="#home"
+											role="tab"
+											aria-controls="home"
+											aria-selected="true"
+										>
+											sold
+										</a>
+									</li>
+									<li
+										className="nav-item"
+										role="presentation"
+									>
+										<a
+											className="nav-link bg-light"
+											id="profile-tab"
+											data-toggle="tab"
+											href="#profile"
+											role="tab"
+											aria-controls="profile"
+											aria-selected="false"
+										>
+											Unsold
+										</a>
+									</li>
+									<li
+										className="nav-item"
+										role="presentation"
+									>
+										<a
+											className="nav-link bg-light"
+											id="contact-tab"
+											data-toggle="tab"
+											href="#contact"
+											role="tab"
+											aria-controls="contact"
+											aria-selected="false"
+										>
+											Purchase History
+										</a>
+									</li>
+								</ul>
+								<div className="tab-content" id="myTabContent">
+									<div
+										className="tab-pane fade show active"
+										id="home"
+										role="tabpanel"
+										aria-labelledby="home-tab"
+									>
+										sold items here
+									</div>
+									<div
+										className="tab-pane fade"
+										id="profile"
+										role="tabpanel"
+										aria-labelledby="profile-tab"
+									>
+										unsold items hee
+									</div>
+									<div
+										className="tab-pane fade"
+										id="contact"
+										role="tabpanel"
+										aria-labelledby="contact-tab"
+									>
+										product history here!
 									</div>
 								</div>
 							</div>
