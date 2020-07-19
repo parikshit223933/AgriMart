4dc48aae913caa37754a796dd3c2baa3ccef1f60
Parikshit Singh <pk223933@gmail.com>
2020-07-19 17:35:28 +0530

    Included deleting product with a modal

--- client/src/components/ProfileSubComponents/SingleProductBox.js
+++ client/src/components/ProfileSubComponents/SingleProductBox.js
@@ -3,25 +3,22 @@ import { API_URLS } from "../../helpers/urls";
 import * as $ from "jquery";
 import { Link } from "react-router-dom";
 import { deleteProduct } from "../../actions/product";
-import {connect} from 'react-redux';
+import { connect } from "react-redux";
 
 class SingleProduct extends React.Component {
 	componentDidMount() {
-        this.handleproductImages();
-        console.log(this.props.product._id);
-    }
-    componentDidUpdate(prevProps, prevState)
-    {
-        this.handleproductImages();
-    }
-    handleDeleteProduct=(productId)=>
-    {   
-        console.log(productId);
-        this.props.dispatch(deleteProduct(productId, this.props.auth.user._id));
-    }
-    handleproductImages=()=>
-    {
-        if (this.props.product) {
+		this.handleproductImages();
+		console.log(this.props.product._id);
+	}
+	componentDidUpdate(prevProps, prevState) {
+		this.handleproductImages();
+	}
+	handleDeleteProduct = (productId) => {
+		console.log(productId);
+		this.props.dispatch(deleteProduct(productId, this.props.auth.user._id));
+	};
+	handleproductImages = () => {
+		if (this.props.product) {
 			const { product } = this.props;
 			let firstPart =
 				API_URLS.profilePictureRoot() + "/uploads/products/coverImage-";
@@ -37,10 +34,10 @@ class SingleProduct extends React.Component {
 			$(`.${this.props.productId}`).css("background-repeat", "no-repeat");
 			$(`.${this.props.productId}`).css("background-size", "cover");
 		}
-    }
-    
+	};
+
 	render() {
-        //if editAllowed is true then i can allow the user to edit the product!
+		//if editAllowed is true then i can allow the user to edit the product!
 		if (this.props.editAllowed) {
 			const { product } = this.props;
 			return (
@@ -71,8 +68,11 @@ class SingleProduct extends React.Component {
 					</div>
 					{/* this is the cover on each product which contains 3 actions, i.e. to delete, edit or view the product. */}
 					<div className="editable-cover">
-						<Link to={`/edit-product/${product._id}/${product.seller}`} className="EDIT">
-                            {/* EDIT BUTTON TO TRIGGER MODAL */}
+						<Link
+							to={`/edit-product/${product._id}/${product.seller}`}
+							className="EDIT"
+						>
+							{/* EDIT BUTTON TO TRIGGER MODAL */}
 							<button
 								type="button"
 								className="btn btn-warning"
@@ -81,10 +81,15 @@ class SingleProduct extends React.Component {
 							>
 								Edit
 							</button>
-                            {/* EDIT MODAL */}
+							{/* EDIT MODAL */}
 						</Link>
 						<div>
-							<button type="button" className="btn btn-danger" id={product._id} onClick={()=>{this.handleDeleteProduct(product._id)}}>
+							<button
+								type="button"
+								className="btn btn-danger"
+								data-toggle="modal"
+								data-target="#deleteProduct"
+							>
 								Delete
 							</button>
 						</div>
@@ -92,6 +97,61 @@ class SingleProduct extends React.Component {
 							<button type="button" className="btn btn-primary">
 								View
 							</button>
+						</div>
+					</div>
+					<div
+						className="modal fade"
+						id="deleteProduct"
+						tabIndex="-1"
+						role="dialog"
+						aria-labelledby="deleteProductLabel"
+						aria-hidden="true"
+					>
+						<div className="modal-dialog modal-dialog-centered">
+							<div className="modal-content">
+								<div className="modal-header">
+									<h5
+										className="modal-title"
+										id="deleteProductLabel"
+									>
+										Delete {product.name}?
+									</h5>
+									<button
+										type="button"
+										className="close"
+										data-dismiss="modal"
+										aria-label="Close"
+									>
+										<span aria-hidden="true">&times;</span>
+									</button>
+								</div>
+								<div className="modal-body">
+									Are you sure you want to delete{" "}
+									{product.name}? <br /> This process can't be
+									undone!
+								</div>
+								<div className="modal-footer">
+									<button
+										type="button"
+										className="btn btn-secondary"
+										data-dismiss="modal"
+									>
+										Cancel
+									</button>
+									<button
+										type="button"
+										className="btn btn-danger"
+										data-dismiss="modal"
+										onClick={() => {
+											this.handleDeleteProduct(
+												product._id
+											);
+										}}
+									>
+										Delete
+									</button>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
@@ -129,10 +189,9 @@ class SingleProduct extends React.Component {
 		);
 	}
 }
-function mapStateToProps({auth})
-{
-    return{
-        auth
-    }
+function mapStateToProps({ auth }) {
+	return {
+		auth
+	};
 }
 export default connect(mapStateToProps)(SingleProduct);
