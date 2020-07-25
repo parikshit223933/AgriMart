2f744b0f58d335d50d4dab474c42bfdd8f0c3e46
Parikshit Singh <pk223933@gmail.com>
2020-07-25 23:56:05 +0530

    categories in home component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,342 @@
 import React from "react";
 import "../home.css";
 import { Link } from "react-router-dom";
+import ScrollMenu from "react-horizontal-scrolling-menu";
+
+// list of items
+const list = [
+	{
+		name: (
+			<Link to="/categories/CerealsAndPulses"
+				className="card"
+				style={{ maxWidth: "400px", maxHeight: "200px" }}
+			>
+				<div className="row no-gutters h-100">
+					<div className="col-4">
+						<div
+							style={{
+								height: 200,
+								backgroundImage: `url('https://img3.stockfresh.com/files/f/finpoints/m/23/7766976_stock-photo-pulses-food-background-assortment---legume-kidney-beans-peas-lentils-in-square-cells-macro.jpg')`,
+								backgroundSize: "cover",
+								backgroundPosition: "center",
+								backgroundRepeat: "no-repeat",
+								maxWidth: 150
+							}}
+						></div>
+					</div>
+					<div className="col-8">
+						<div className="card-body">
+							<h5 className="card-title">Cereals And Pulses</h5>
+							<p className="card-text">
+								This includes Barley, Wheat, Maize, Rice, Rye,
+								Pinto Bean, Garden Pea, ChickPea, Velvet Bean,
+								Ground Nuts, e.t.c.
+							</p>
+						</div>
+					</div>
+				</div>
+			</Link>
+		)
+	},
+	{
+		name: (
+			<Link to="/categories/Seeds"
+				className="card"
+				style={{ maxWidth: "400px", maxHeight: "200px" }}
+			>
+				<div className="row no-gutters">
+					<div className="col-4">
+						<div
+							style={{
+								height: 200,
+								backgroundImage: `url('https://5.imimg.com/data5/ED/FU/MY-1051022/vegetable-seeds-500x500.jpeg')`,
+								backgroundSize: "cover",
+								backgroundPosition: "center",
+								backgroundRepeat: "no-repeat",
+								maxWidth: 150
+							}}
+						></div>
+					</div>
+					<div className="col-8">
+						<div className="card-body">
+							<h5 className="card-title">Seeds</h5>
+							<p className="card-text">
+								This includes Basil Seeds, Cumin Seeds, Dill
+								Seeds, Mustard Seeds, Sunflower Seeds, Linseed,
+								Cotton Seeds, e.t.c.
+							</p>
+						</div>
+					</div>
+				</div>
+			</Link>
+		)
+	},
+	{
+		name: (
+			<Link to="/categories/Spices"
+				className="card"
+				style={{ maxWidth: "400px", maxHeight: "200px" }}
+			>
+				<div className="row no-gutters">
+					<div className="col-4">
+						<div
+							style={{
+								height: 200,
+								backgroundImage: `url('https://5.imimg.com/data5/EK/LK/MY-24207714/indian-spices-500x500.jpg')`,
+								backgroundSize: "cover",
+								backgroundPosition: "center",
+								backgroundRepeat: "no-repeat",
+								maxWidth: 150
+							}}
+						></div>
+					</div>
+					<div className="col-8">
+						<div className="card-body">
+							<h5 className="card-title">Spices</h5>
+							<p className="card-text">
+								This includes Black Pepper, Salt, Chilli Powder,
+								Cinnamon, Cloves, Coriander, Cumin, Curry
+								Powder, Ginger, Dry Red Chilly, Chat Masala,
+								e.t.c.
+							</p>
+						</div>
+					</div>
+				</div>
+			</Link>
+		)
+	},
+	{
+		name: (
+			<Link to="/categories/Fruits"
+				className="card"
+				style={{ maxWidth: "400px", maxHeight: "200px" }}
+			>
+				<div className="row no-gutters">
+					<div className="col-4">
+						<div
+							style={{
+								height: 200,
+								backgroundImage: `url('https://img.etimg.com/thumb/width-640,height-480,imgsize-322201,resizemode-1,msid-75714944/magazines/panache/doctors-suggest-having-fruits-could-help-keep-coronavirus-at-bay/doctors-also-stressed-the-need-to-continue-to-take-precautions-such-as-having-a-good-diet-and-trying-to-keep-away-from-polluted-or-smoky-areas-.jpg')`,
+								backgroundSize: "cover",
+								backgroundPosition: "center",
+								backgroundRepeat: "no-repeat",
+								maxWidth: 150
+							}}
+						></div>
+					</div>
+					<div className="col-8">
+						<div className="card-body">
+							<h5 className="card-title">Fruits</h5>
+							<p className="card-text">
+								This includes Banana, Apple, Mango, Guava,
+								Grapes, Apricot, BlueBerry, Strawberry, Jamun,
+								Oranges, Water Melon, e.t.c.
+							</p>
+						</div>
+					</div>
+				</div>
+			</Link>
+		)
+	},
+	{
+		name: (
+			<Link to="/categories/Vegetables" 
+				className="card"
+				style={{ maxWidth: "400px", maxHeight: "200px" }}
+			>
+				<div className="row no-gutters">
+					<div className="col-4">
+						<div
+							style={{
+								height: 200,
+								backgroundImage: `url('https://www.healthyeating.org/images/default-source/home-0.0/nutrition-topics-2.0/general-nutrition-wellness/2-2-2-2foodgroups_vegetables_detailfeature.jpg?sfvrsn=226f1bc7_6')`,
+								backgroundSize: "cover",
+								backgroundPosition: "center",
+								backgroundRepeat: "no-repeat",
+								maxWidth: 150
+							}}
+						></div>
+					</div>
+					<div className="col-8">
+						<div className="card-body">
+							<h5 className="card-title">Vegetables</h5>
+							<p className="card-text">
+								This includes Carrots, Lady Finger, French
+								Beans, Potato, Cucumber, Msuhroom, Bell Pepper,
+								Bitter Gourd, Leek, Peas, e.t.c.
+							</p>
+						</div>
+					</div>
+				</div>
+			</Link>
+		)
+	},
+	{
+		name: (
+			<Link to="/categories/DryFruits"
+				className="card"
+				style={{ maxWidth: "400px", maxHeight: "200px" }}
+			>
+				<div className="row no-gutters">
+					<div className="col-4">
+						<div
+							style={{
+								height: 200,
+								backgroundImage: `url('https://images-eu.ssl-images-amazon.com/images/I/61f%2BBoiUTGL._SY300_QL70_ML2_.jpg')`,
+								backgroundSize: "cover",
+								backgroundPosition: "center",
+								backgroundRepeat: "no-repeat",
+								maxWidth: 150
+							}}
+						></div>
+					</div>
+					<div className="col-8">
+						<div className="card-body">
+							<h5 className="card-title">Dry Fruits</h5>
+							<p className="card-text">
+								This includes Cashew Nut, Almond, Raisins,
+								Pistachio, Peanut, Apricot, Dried Peaches, Figs,
+								Pumpkin Seeds, Pine Nuts, e.t.c.
+							</p>
+						</div>
+					</div>
+				</div>
+			</Link>
+		)
+	},
+	{
+		name: (
+			<Link to="/categories/EdibleOils"
+				className="card"
+				style={{ maxWidth: "400px", maxHeight: "200px" }}
+			>
+				<div className="row no-gutters">
+					<div className="col-4">
+						<div
+							style={{
+								height: 200,
+								backgroundImage: `url('https://m.economictimes.com/thumb/height-450,width-600,imgsize-199230,msid-76194134/edible-oil.jpg')`,
+								backgroundSize: "cover",
+								backgroundPosition: "center",
+								backgroundRepeat: "no-repeat",
+								maxWidth: 150
+							}}
+						></div>
+					</div>
+					<div className="col-8">
+						<div className="card-body">
+							<h5 className="card-title">Edible Oils</h5>
+							<p className="card-text">
+								This includes Castor Oil, Coconut Oil, Vegetable
+								Oil, Cumin Seed Oil, Celery Seed Oil, Mustard
+								Oil, Sunflower Oil, Cotton Seed Oil, e.t.c.
+							</p>
+						</div>
+					</div>
+				</div>
+			</Link>
+		)
+	},
+	{
+		name: (
+			<Link to="/categories/DairyProducts"
+				className="card"
+				style={{ maxWidth: "400px", maxHeight: "200px" }}
+			>
+				<div className="row no-gutters">
+					<div className="col-4">
+						<div
+							style={{
+								height: 200,
+								backgroundImage: `url('https://www.healthline.com/hlcmsresource/images/AN_images/AN480-Eggs-Dairy-732x549-thumb.jpg')`,
+								backgroundSize: "cover",
+								backgroundPosition: "center",
+								backgroundRepeat: "no-repeat",
+								maxWidth: 150
+							}}
+						></div>
+					</div>
+					<div className="col-8">
+						<div className="card-body">
+							<h5 className="card-title">Dairy Products</h5>
+							<p className="card-text">
+								This includes Cheese Spread, Milk, Condensed
+								Milk, Powdered Milk, Cream, Ice Cream, Cottage
+								Cheese (Paneer), e.t.c.
+							</p>
+						</div>
+					</div>
+				</div>
+			</Link>
+		)
+	},
+	{
+		name: (
+			<Link to="/categories/Others"
+				className="card"
+				style={{ maxWidth: "400px", maxHeight: "200px" }}
+			>
+				<div className="row no-gutters">
+					<div className="col-4">
+						<div
+							style={{
+								height: 200,
+								backgroundImage: `url('https://i0.wp.com/cdn-prod.medicalnewstoday.com/content/images/articles/317/317728/honey-in-a-pot.jpg?w=1155&h=1541')`,
+								backgroundSize: "cover",
+								backgroundPosition: "center",
+								backgroundRepeat: "no-repeat",
+								maxWidth: 150
+							}}
+						></div>
+					</div>
+					<div className="col-8">
+						<div className="card-body">
+							<h5 className="card-title">Others</h5>
+							<p className="card-text">
+								This includes Natural Honey, Sugar, Soya Meals,
+								Jaggery, Natural Dyes and Pigments, Dehydrated
+								vegetables, Vinegar, e.t.c.
+							</p>
+						</div>
+					</div>
+				</div>
+			</Link>
+		)
+	}
+];
+
+const MenuItem = ({ text }) => {
+	return <div className={`menu-item`}>{text}</div>;
+};
+
+export const Menu = (list) =>
+	list.map((el, index) => {
+		const { name } = el;
+
+		return <MenuItem text={name} key={index} />;
+	});
+
+const Arrow = ({ text, className }) => {
+	return <div className={className}>{text}</div>;
+};
+
+const ArrowLeft = Arrow({
+	text: <i className="fas fa-angle-left"></i>,
+	className: "arrow-prev"
+});
+const ArrowRight = Arrow({
+	text: <i className="fas fa-angle-right"></i>,
+	className: "arrow-next"
+});
+
 class Home extends React.Component {
+	constructor(props) {
+		super(props);
+		// call it again if items count changes
+		this.menuItems = Menu(list);
+	}
 	render() {
+		const menu = this.menuItems;
 		return (
 			<div className="home-component">
 				<div className="grad-cover">
@@ -59,9 +393,19 @@ class Home extends React.Component {
 						</div>
 					</div>
 				</div>
-                <div>
-                    
-                </div>
+				<div className="hrzntl-scrll mb-5 pt-3 pb-5">
+                    <p className="display-4 pl-3">Categories</p>
+					<ScrollMenu
+						data={menu}
+						arrowLeft={ArrowLeft}
+						arrowRight={ArrowRight}
+						alignCenter={false}
+						alignOnResize={false}
+						wheel={false}
+						hideArrows={true}
+						scrollBy={1}
+					/>
+				</div>
 			</div>
 		);
 	}
